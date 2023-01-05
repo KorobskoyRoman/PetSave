dff1dbf862c90eb23e2f0cffd726d3f60f02b95e
KorobskoyRoman <shopone023@gmail.com>
2023-01-05 17:22:44 +0300

    feat: Add animal model

---
+++ PetSave/Core/domain/model/animal/Animal.swift
@@ -0,0 +1,34 @@
+//
+//  Animal.swift
+//  PetSave
+//
+//  Created by Roman Korobskoy on 04.01.2023.
+//
+
+import Foundation
+
+struct Animal: Codable {
+    var id: Int?
+    let organizationId: String?
+    let url: URL?
+    let type: String
+    let species: String?
+    var breeds: Breed
+    var colors: APIColors
+    let age: Age
+    let gender: Gender
+    let size: Size
+    let coat: Coat?
+    let name: String
+    let description: String?
+    let photos: [PhotoSizes]
+    let videos: [VideoLink]
+    let status: AdoptionStatus
+    var attributes: AnimalAttributes
+    var environment: AnimalEnvironment?
+    let tags: [String]
+    var contact: Contact
+    let publishedAt: String?
+    let distance: Double?
+    var ranking: Int? = 0
+}
