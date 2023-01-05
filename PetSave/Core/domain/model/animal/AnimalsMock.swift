67dc58ff00f9b564ed92157091fbebb54db66c53
KorobskoyRoman <shopone023@gmail.com>
2023-01-05 17:23:02 +0300

    feat: Configure mocks

---
+++ PetSave/Core/domain/model/animal/AnimalsMock.swift
@@ -0,0 +1,29 @@
+//
+//  AnimalsMock.swift
+//  PetSave
+//
+//  Created by Roman Korobskoy on 04.01.2023.
+//
+
+import Foundation
+
+private struct AnimalsMock: Codable {
+    let animals: [Animal]
+}
+
+private func loadAnimals() -> [Animal] {
+    guard let url = Bundle.main.url(
+        forResource: "AnimalsMock",
+        withExtension: "json"
+    ), let data = try? Data(contentsOf: url) else { return [] }
+
+    let decoder = JSONDecoder()
+
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    let jsonMock = try? decoder.decode(AnimalsMock.self, from: data)
+    return jsonMock?.animals ?? []
+}
+
+extension Animal {
+    static let mock = loadAnimals()
+}
