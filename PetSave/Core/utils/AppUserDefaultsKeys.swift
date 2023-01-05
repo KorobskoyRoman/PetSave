63143c3cf2de8701cf2471ecc4bbebb1226cd865
KorobskoyRoman <shopone023@gmail.com>
2023-01-05 19:49:20 +0300

    feat: Add keys for UD

---
+++ PetSave/Core/utils/AppUserDefaultsKeys.swift
@@ -0,0 +1,11 @@
+//
+//  AppUserDefaultsKeys.swift
+//  PetSave
+//
+//  Created by Roman Korobskoy on 05.01.2023.
+//
+
+enum AppUserDefaultsKeys {
+  static let expiresAt = "expiresAt"
+  static let bearerAccessToken = "bearerAccessToken"
+}
