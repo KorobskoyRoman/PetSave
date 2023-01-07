92e871e3c53e6870721ed4a2b67a6402e2fce03e
KorobskoyRoman <shopone023@gmail.com>
2023-01-07 05:27:24 +0300

    feat: Core data tests

--- PetSaveTests/Tests/Core/data/coreData/CoreDataTests.swift
+++ PetSaveTests/Tests/Core/data/coreData/CoreDataTests.swift
@@ -35,11 +35,55 @@ import XCTest
 import CoreData
 
 class CoreDataTests: XCTestCase {
-  override func setUpWithError() throws {
-    try super.setUpWithError()
-  }
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
 
-  override func tearDownWithError() throws {
-    try super.tearDownWithError()
-  }
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func testTooManagedObject() throws {
+        let previewContext = PersistenceController.preview.container.viewContext
+
+        let fetchRequest = AnimalEntity.fetchRequest()
+
+        fetchRequest.fetchLimit = 1
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: \AnimalEntity.name, ascending: true)]
+
+        guard let results = try? previewContext.fetch(fetchRequest),
+              let first = results.first else { return }
+
+        XCTAssert(first.name == "CHARLA", """
+         Pet name did not match, was expecting Kiki, got
+         \(String(describing: first.name))
+         """)
+        XCTAssert(first.type == "Dog", """
+         Pet type did not match, was expecting Cat, got
+         \(String(describing: first.type))
+         """)
+        XCTAssert(first.coat.rawValue == "Short", """
+         Pet coat did not match, was expecting Short, got
+         \(first.coat.rawValue)
+         """)
+    }
+
+    func testDeleteManagedObject() throws {
+        let previewContext = PersistenceController.preview.container.viewContext
+
+        let fetchRequest = AnimalEntity.fetchRequest()
+
+        guard let results = try? previewContext.fetch(fetchRequest),
+              let first = results.first else { return }
+
+        let expectedResults = results.count - 1
+        previewContext.delete(first)
+
+        guard let resltsAfterDeletion = try? previewContext.fetch(fetchRequest) else { return }
+
+        XCTAssert(expectedResults, resltsAfterDeletion.count, """
+        The number of results was expected to be \(expectedResult)
+        after deletion, was \(results.count)
+        """)
+    }
 }
