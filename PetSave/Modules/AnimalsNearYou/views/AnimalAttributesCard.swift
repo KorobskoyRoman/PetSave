c6bca9e1f17ac0c2c24a4e9d1ff12ab696a6a5c1
KorobskoyRoman <shopone023@gmail.com>
2023-01-09 19:46:16 +0300

    feat: View model unit tests

---
+++ PetSave/Modules/AnimalsNearYou/views/AnimalAttributesCard.swift
@@ -0,0 +1,8 @@
+//
+//  AnimalAttributesCard.swift
+//  PetSave
+//
+//  Created by Roman Korobskoy on 08.01.2023.
+//
+
+import Foundation
---
+++ PetSaveTests/Tests/AnimalsNearYou/AnimalsNearYouViewModelTestCase.swift
@@ -0,0 +1,58 @@
+//
+//  AnimalsNearYouViewModelTestCase.swift
+//  PetSaveTests
+//
+//  Created by Roman Korobskoy on 08.01.2023.
+//
+
+import XCTest
+
+@testable import PetSave
+
+@MainActor
+final class AnimalsNearYouViewModelTestCase: XCTestCase {
+    let testContext = PersistenceController.shared.container.viewContext
+
+    var viewModel: AnimalsNearYouViewModel!
+
+    @MainActor
+    override func setUp() {
+        super.setUp()
+
+        viewModel = AnimalsNearYouViewModel(
+            isLoading: true,
+            animalFetcher: AnimalsFetcherMock(),
+            animalStorage: AnimalStorageService(context: testContext)
+        )
+    }
+
+    func testFetchAnimalsLoadingState() async {
+        XCTAssertTrue(viewModel.isLoading, "View model should be loading, but it isn't")
+        await viewModel.fetchAnimals()
+        XCTAssertFalse(viewModel.isLoading, "View model should'nt be loading, but it is")
+    }
+
+    func testUpdatePageOnFetchMoreAnimals() async {
+        XCTAssertEqual(viewModel.page, 1, "Page should be 1, but it's \(viewModel.page)")
+        await viewModel.fetchMoreAnimals()
+        XCTAssertEqual(viewModel.page, 2, "Page should be 2, but it's \(viewModel.page)")
+    }
+
+    func testFetchAnimalsEmptyResponse() async {
+        viewModel = AnimalsNearYouViewModel(
+            isLoading: true,
+            animalFetcher: EmptyResponseAnimalsFetcherMock(),
+            animalStorage: AnimalStorageService(context: testContext)
+        )
+        await viewModel.fetchAnimals()
+
+        XCTAssertFalse(viewModel.hasMoreAnimals, "hasMoreAnimals should be false, but it's true")
+        XCTAssertFalse(viewModel.isLoading, "isLoading should be false, but it's true")
+    }
+}
+
+struct EmptyResponseAnimalsFetcherMock: AnimalsFetcher {
+    func fetchAnimals(page: Int) async -> [Animal] {
+        return []
+    }
+}
