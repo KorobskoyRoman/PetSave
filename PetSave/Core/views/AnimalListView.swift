6a6d38abfd2f40887db23cbc9562fa5ebb88d0ef
KorobskoyRoman <shopone023@gmail.com>
2023-01-25 00:11:18 +0300

    feat: Universal list view

---
+++ PetSave/Core/views/AnimalListView.swift
@@ -0,0 +1,52 @@
+//
+//  AnimalListView.swift
+//  PetSave
+//
+//  Created by Roman Korobskoy on 24.01.2023.
+//
+
+import SwiftUI
+
+struct AnimalListView<Content, Data>: View where Content: View,
+                                                 Data: RandomAccessCollection,
+                                                 Data.Element: AnimalEntity {
+    let animals: Data
+    let footer: Content
+
+    init(animals: Data, @ViewBuilder footer: () -> Content) {
+        self.animals = animals
+        self.footer = footer()
+    }
+
+    init(animals: Data) where Content == EmptyView {
+        self.init(animals: animals) {
+            EmptyView()
+        }
+    }
+
+    var body: some View {
+        List {
+            ForEach(animals) { animal in
+                NavigationLink(destination: AnimalDetailsView()) {
+                    AnimalRow(animal: animal)
+                }
+            }
+            footer
+        }
+        .listStyle(.plain)
+    }
+}
+
+struct AnimalListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AnimalListView(animals: CoreDataHelper.getTestAnimalEntities() ?? [])
+        }
+
+        NavigationView {
+            AnimalListView(animals: []) {
+                Text("Footer")
+            }
+        }
+    }
+}
