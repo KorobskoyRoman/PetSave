1d57dbb2b3e64c9c4451f9dfc75a9e3f3abfcbce
KorobskoyRoman <shopone023@gmail.com>
2023-01-26 17:43:26 +0300

    feat: Onboarding framework

---
+++ PetSaveOnboarding/Extensions/Bundle+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Bundle+Extensions.swift
+//  PetSaveOnboarding
+//
+//  Created by Roman Korobskoy on 25.01.2023.
+//
+
+import Foundation
+
+extension Bundle {
+    public static var module: Bundle? {
+        Bundle(identifier: "com.underscoreow.PetSaveOnboarding")
+    }
+}
